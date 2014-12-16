2c30cd0bff4325ca6dede42103b5647f1a256437
mlangenmaier <martinlangenmaier@googlemail.com>
2014-12-16 16:36:04 +0100

    header incss übertragen

--- header.php
+++ header.php
@@ -21,12 +21,10 @@
 <div id="page" class="hfeed site">
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', '_s' ); ?></a>
 
-	<header  style="background-color: aqua;" class="site-header" role="banner">
+	<header class="site-header" role="banner">
 		<div class="site-branding">
-			<div style="text-align: center;">
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-			</div>
+			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
