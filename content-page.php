f6da48f99c7d31c7d94e599ae87c5de3193662ab
mlangenmaier <martinlangenmaier@googlemail.com>
2014-12-26 15:28:25 +0100

    Revert "kommentarfeld enfernt"
    This reverts commit 43a4fe64838183dee0f7c8c97c0d2e0cd84d69b0.

--- content-page.php
+++ content-page.php
@@ -21,4 +21,7 @@
 		?>
 	</div><!-- .entry-content -->
 
+	<footer class="entry-footer">
+		<?php edit_post_link( __( 'Edit', '_s' ), '<span class="edit-link">', '</span>' ); ?>
+	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
